9dc28cec5e089849d2ef689fb9e7719189861543
Brad Phillips <bjrp@protonmail.com>
2019-10-05 11:08:05 +0100

    Replaces global variable with class variable

--- app.rb
+++ app.rb
@@ -8,6 +8,10 @@ class Battle < Sinatra::Base
 
 enable :sessions
 
+before do
+  @game = Game.instance
+end
+
 
   get '/' do
     erb(:index)
@@ -16,26 +20,24 @@ enable :sessions
   post '/names' do
     player_1 = Player.new(params[:player_1])
     player_2 = Player.new(params[:player_2])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
 
     redirect '/play'
   end
 
 
   get '/play' do
-    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @game = $game
     @game.attack(@game.invert(@game.current_turn))
     erb(:attack)
   end
 
   post '/swap_turns' do
-    $game.swap_turns
-    if $game.defeated?
+    @game.swap_turns
+    if @game.defeated?
       redirect '/gameover'
     else
       redirect '/play'
